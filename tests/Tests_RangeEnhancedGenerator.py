4af24ca7868717211362a2a5d9e7ef33aab114a8
StudentMAGe <43290436+StudentMAGe@users.noreply.github.com>
2019-03-12 21:14:48 +0300

    Test changed according to Reviewer's comments

--- tests/Tests_RangeEnhancedGenerator.py
+++ tests/Tests_RangeEnhancedGenerator.py
@@ -1,48 +1,20 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
 import unittest
-import RangeEnhancedIterator_Generator
 from RangeEnhancedIterator_Generator import GeneratorEnhancedRange
 
 
 class GeneratorTests(unittest.TestCase):
 
-    def setUp(self):
-        print ('Setup called')
-    
     def test_on_range_w_one_arg(self):
-        example = GeneratorEnhancedRange(2)
-        s = []
-        for i in range(10):
-            for i in example():
-                s.append(i)
-        self.assertEqual(s, [0, 1])
-    
+        self.assertEqual([n for n in GeneratorEnhancedRange(2)], [0, 1])
+
     def test_on_range_w_two_args(self):
-        example = GeneratorEnhancedRange(2,5)
-        s = []
-        for i in range(10):
-            for i in example():
-                s.append(i)
-        self.assertEqual(s, [2, 3, 4])
-            
+        self.assertEqual([n for n in GeneratorEnhancedRange(2, 5)], [2, 3, 4])
+
     def test_on_range_w_three_args(self):
-        example = GeneratorEnhancedRange(2, 7, 2)
-        s = []
-        for i in range(10):
-            for i in example():
-                s.append(i)
-        self.assertEqual(s, [2, 4, 6])
-          
-    def tearDown(self):
-        print ('Tear down called')
+        self.assertEqual([n for n in GeneratorEnhancedRange(2, 7, 2)], [2, 4, 6])
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
+    
+    
