ffd96ece7811a7f7eb8b7bdfe8df4e8a4e56314c
StudentMAGe <43290436+StudentMAGe@users.noreply.github.com>
2019-03-13 16:01:49 +0300

    Added several additional tests

--- tests/Tests_RangeEnhancedGenerator.py
+++ tests/Tests_RangeEnhancedGenerator.py
@@ -12,6 +12,22 @@ class GeneratorTests(unittest.TestCase):
 
     def test_on_range_w_three_args(self):
         self.assertEqual([n for n in GeneratorEnhancedRange(2, 7, 2)], [2, 4, 6])
+        
+    def test_on_nonnumeric_args(self):
+        with self.assertRaises(Exception, msg = "Arguments must be numeric"):
+            example = GeneratorEnhancedRange(5, "o", -1)
+
+    def test_on_zero_step(self):
+        with self.assertRaises(Exception, msg = "Unable to range with step = 0"):
+            example = GeneratorEnhancedRange(5, 6, 0)
+
+    def test_on_wrong_number_of_args(self):
+        with self.assertRaises(Exception,
+                msg = "GeneratorEnhancedRange requires at least one, but no more than three arguments"):
+            example = GeneratorEnhancedRange()
+        with self.assertRaises(Exception,
+                msg = "GeneratorEnhancedRange requires at least one, but no more than three arguments"):
+            example = GeneratorEnhancedRange(2, 7, 2, 0)
 
 
 if __name__ == '__main__':
